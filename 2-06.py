ae106b63cf8697a8842d322e2f726f9eb35d1fca
Mir090119 <rahmonbek40@gmail.com>
2025-07-02 15:18:44 +0500

    2-07

---
+++ 2-06.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+# q = datetime.datetime.now()
+# print(q)
+# =-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-==-=\\
+
+
+# date1_str = input()
+# date2_str = input()
+# date1 = datetime.strptime(date1_str, "%Y-%m-%d")
+# date2 = datetime.strptime(date2_str, "%Y-%m-%d")
+# print((date2 - date1).days)
+
+
+
+
+# date_str = input()
+# date_week = datetime.strptime(date_str, "%Y-%m-%d")
+# print(date_week.strftime("%A"))
+
+
+# date_str = input()
+
+# date1 = datetime.strptime(date_str, "%Y-%m-%d")
+
+# print(date1.strftime("%d %B %Y"))
